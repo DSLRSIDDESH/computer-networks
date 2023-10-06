ba19318d74d2c79d8aa34631df45fbfda2f2c556
Siddesh Devarakonda <siddeshdslr@gmail.com>
2023-10-06 10:15:30 +0530

    Implementation Completed

--- lab-08/client.py
+++ lab-08/client.py
@@ -1,19 +1,18 @@
 # CS21B2019 DSLR SIDDESH
 # client.py
-
 import socket
 import pygame
 import pickle
+import os
 
 import tkinter as tk
 from tkinter import messagebox
 from getmac import get_mac_address
 
 IP = ''
-PORT = 8001
+PORT = 8100
 ADDR = (IP, PORT)                                           
 SIZE = 4096
-FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "DISCONNECT"
 CONNECTED = False
 
@@ -28,7 +27,7 @@ COIN_COLOR = (137, 207, 240)
 def game_entry():
     def register():
         mac = mac_entry.get()
-        client.send(pickle.dumps(f"REGISTER/{mac};"))
+        client.send(pickle.dumps(f"REGISTER/{mac}"))
         msg = recv_msg(client)
         if msg == "OK":
             print(f"Registered MAC Address: {mac}")
@@ -39,7 +38,7 @@ def game_entry():
     
     def login():
         mac = mac_entry.get()
-        client.send(f"LOGIN/{mac};".encode(FORMAT))
+        client.send(pickle.dumps(f"LOGIN/{mac}"))
         msg = recv_msg(client)
         if msg == "OK":
             print(f"Logged in with MAC Address: {mac}")
@@ -51,7 +50,7 @@ def game_entry():
     def pay():
         mac = mac_entry.get()
         amount = amount_entry.get()
-        client.send(f"PAY/{mac}/{amount};".encode(FORMAT))
+        client.send(pickle.dumps(f"PAY/{mac}/{amount}"))
         msg = recv_msg(client)
         if msg == "OK":
             print(f"Paid {amount} for MAC Address: {mac}")
@@ -107,8 +106,9 @@ def send_player_input(client):
 def receive_game_state(client):
     try:
         data = recv_msg(client)
-        game_state = pickle.loads(data)
-        return game_state
+        # print(data)
+        # game_state = pickle.loads(data)
+        return data
     except Exception as e:
         print(f"Error receiving game state: {e}")
         return None
@@ -125,7 +125,7 @@ def render_players_scores(game_state):
 def disconnect_server(client: socket.socket, recv_from: str):
     global connected
     connected = False
-    client.send(DISCONNECT_MESSAGE.encode(FORMAT))
+    client.send(pickle.dumps(DISCONNECT_MESSAGE))
     if recv_from == "client":
         print(f"[DISCONNECTED] Client disconnected from {IP}:{PORT}")
     elif recv_from == "server":
@@ -133,56 +133,60 @@ def disconnect_server(client: socket.socket, recv_from: str):
     client.close()
 
 def recv_msg(client: socket.socket, disconnect_info: str = ""):
-    msg = client.recv(SIZE).decode(FORMAT)
+    msg = client.recv(SIZE)
+    if not msg:
+        return ''
     msg = pickle.loads(msg)
     if msg == DISCONNECT_MESSAGE:
         print(disconnect_info)
         disconnect_server(client, "server")
     return msg
 
-if __name__ == "__main__":
+try:
+    if __name__ == "__main__":
+        client.connect(ADDR)
+        print(f"> [CONNECTED] Client connected to server at {IP}:{PORT}")
 
-    client.connect(ADDR)
-    print(f"[CONNECTED] Client connected to server at {IP}:{PORT}")
+        game_entry()
 
-    game_entry()
+        pygame.init()
+        screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption("Coin Collector Game")
 
-    pygame.init()
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    pygame.display.set_caption("Coin Collector Game")
+        connected = True
+        while connected:    
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    connected = False
 
-    connected = True
-    while connected:    
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                connected = False
-
-        send_player_input(client)
-        game_state = receive_game_state(client)  
-        player_id = 0
-        
-        if type(game_state) == int:
-            font = pygame.font.Font('freesansbold.ttf', 32)
-            text = font.render(f'Player-{game_state + 1} Won!', True, (255, 255, 255))
-            textRect = text.get_rect()
-            textRect.center = (WIDTH // 2, HEIGHT // 2)
-            screen.blit(text, textRect)
-            pygame.display.flip()
-            pygame.time.delay(50000)
+            send_player_input(client)
+            game_state = receive_game_state(client)
             
-        else:
-            if game_state != None:
-                coins = game_state['coins']
+            if type(game_state) in (int, str):
+                pass
+            #     font = pygame.font.Font('freesansbold.ttf', 32)
+            #     text = font.render(f'Player-{game_state} Won!', True, (255, 255, 255))
+            #     textRect = text.get_rect()
+            #     textRect.center = (WIDTH // 2, HEIGHT // 2)
+            #     screen.blit(text, textRect)
+            #     pygame.display.flip()
+            #     pygame.time.delay(50000)
                 
-                screen.fill(BACKGROUND) 
+            else:
+                if game_state != None:
+                    coins = game_state['coins']
+                    
+                    screen.fill(BACKGROUND) 
 
-                for player_id, (player_x, player_y) in game_state['players'].items():     
-                    player_color = game_state['color'][player_id]
-                    pygame.draw.rect(screen, player_color, (player_x, player_y, PLAYER_SIZE, PLAYER_SIZE))
-                for coin_x, coin_y in coins:                                
-                    pygame.draw.ellipse(screen, COIN_COLOR, (coin_x, coin_y, COIN_SIZE, COIN_SIZE))
+                    for player_id, (player_x, player_y) in game_state['players'].items():
+                        player_color = game_state['color'][player_id]
+                        pygame.draw.rect(screen, player_color, (player_x, player_y, PLAYER_SIZE, PLAYER_SIZE))
+                    for coin_x, coin_y in coins:                                
+                        pygame.draw.ellipse(screen, COIN_COLOR, (coin_x, coin_y, COIN_SIZE, COIN_SIZE))
 
-                render_players_scores(game_state)
-                pygame.display.flip()
+                    render_players_scores(game_state)
+                    pygame.display.flip()
 
-    pygame.quit()
+        pygame.quit()
+except KeyboardInterrupt:
+    os._exit(1)
--- lab-08/server.py
+++ lab-08/server.py
@@ -1,21 +1,27 @@
-# CS21B2019
-# DSLR SIDDESH
+# CS21B2019 DSLR SIDDESH
 # server.py
 import socket
 import threading
 import random
 import pickle
-import queue
 import time
+import os
+from dataclasses import dataclass
 
-# Server configuration
 IP = ''
-PORT = 8001
+PORT = 8100
 ADDR = (IP, PORT)
 SIZE = 4096
-clients = {}
 
-# Constants
+clients = {}
+players = []
+queue = []
+
+logged_in_macs = []
+active_clients = 0
+max_players = 4
+game_price = 60/100
+
 WIDTH, HEIGHT = 600, 400
 PLAYER_SPEED = 1.5
 PLAYER_SIZE = 30
@@ -24,16 +30,91 @@ COIN_COUNT = 10
 MAX_PLAYERS = 4
 WINNING_SCORE = 10
 
-# Create the game state
 game_state = {
-    'players': {},          # Store player positions as {player_id: (x, y)}
-    'player_scores': {},    # Store player scores as {player_id: score}
+    'players': {},          
+    'player_scores': {},    
     'coins': [(random.randint(0, WIDTH - COIN_SIZE), random.randint(0, HEIGHT - COIN_SIZE)) for _ in range(COIN_COUNT)],
-    'color': {}             # Store player color as {'player_id': (R, G, B)}
+    'color': {}             
 }
 
-# Function to update player position based on input
+@dataclass
+class Client:
+    '''Client class to store client information'''
+    conn: socket.socket
+    addr: str
+    player: int
+    mac: str = None
+    time: float = 0.0
+    connected: bool = True
+
+def get_next_player():
+    all_players = sorted([c.player for c in players])
+    player = 1
+    for p in all_players:
+        if p == player:
+            player += 1
+        else:
+            break
+    return player
+
+def disconnect_client(client: Client):
+    global active_clients
+    print(f"[DISCONNECT CLIENT] {client.addr} disconnected.")
+
+    active_clients -= 1
+    print(f"[ACTIVE CLIENTS] {active_clients}")
+
+    if client in players:
+        players.remove(client)
+        game_state['players'].pop(client.player)
+        game_state['player_scores'].pop(client.player)
+        game_state['color'].pop(client.player)
+        # initialize_player_scores()
+    else:
+        queue.remove(client)
+    client.connected = False
+    try:
+        logged_in_macs.remove(client.mac)
+        client.conn.close()
+    except:
+        pass
+
+def player_timer(client: Client):
+    while client.connected:
+        client.time -= 1
+        if client.time <= 0:
+            client.conn.send(pickle.dumps("TIMEOUT"))
+            disconnect_client(client)
+            break
+        time.sleep(0.1)
+
+def add_player(client: Client):
+    logged_in_macs.append(client.mac)
+
+    if len(players) > max_players:  # If players are full
+        client.player = -1
+        queue.append(client)
+    else:
+        client.player = get_next_player()
+        players.append(client)
+        # Initialize player's position
+        player_x = random.randint(0, WIDTH - PLAYER_SIZE)
+        player_y = random.randint(0, HEIGHT - PLAYER_SIZE)
+        game_state['players'][client.player] = (player_x, player_y)
+
+        
+        display_color = (random.randint(10, 255) for _ in range(3))
+        game_state['color'][client.player] = tuple(display_color)
+        game_state['player_scores'][client.player] = game_state['player_scores'].get(client.player, 0)
+        timer_thread = threading.Thread(target=player_timer, args=(client,))
+        timer_thread.start()
+    
+    client.conn.send(pickle.dumps(client.player))
+    time.sleep(0.1)
+
 def update_player_position(player_id, input_data):
+    global game_state
+    # print(game_state['players'])
     player_x, player_y = game_state['players'][player_id]
     if input_data['left']:
         player_x -= PLAYER_SPEED
@@ -43,97 +124,121 @@ def update_player_position(player_id, input_data):
         player_y -= PLAYER_SPEED
     if input_data['down']:
         player_y += PLAYER_SPEED
-
-    # Ensure the player stays within the game bounds
     player_x = max(0, min(player_x, WIDTH - PLAYER_SIZE))
     player_y = max(0, min(player_y, HEIGHT - PLAYER_SIZE))
 
     game_state['players'][player_id] = (player_x, player_y)
 
-# Function to handle a client
-def handle_client(conn, player_id):
+def handle_client(client: Client):
+    addr = client.addr
+    conn = client.conn
+    print(f"> [NEW CONNECTION] {addr} connected.")
+
     while True:
         data = conn.recv(SIZE)
         if not data:
             break
-        # Update the game state for the player based on input
         input_data = pickle.loads(data)
-        update_player_position(player_id, input_data)
-
-        # Check for collisions with coins
-        coins_to_remove = []
-        for i, (coin_x, coin_y) in enumerate(game_state['coins']):
-            player_x, player_y = game_state['players'][player_id]
-            if player_x < coin_x + COIN_SIZE and player_x + PLAYER_SIZE > coin_x and player_y < coin_y + COIN_SIZE and player_y + PLAYER_SIZE > coin_y:
-                coins_to_remove.append(i)
-                game_state['player_scores'][player_id] = game_state['player_scores'].get(player_id, 0) + 1
-
-        # Remove the coins that were collected and add new one
-        for i in coins_to_remove:
-            game_state['coins'].pop(i)
-            new_coin_x = random.randint(0, WIDTH - COIN_SIZE)
-            new_coin_y = random.randint(0, HEIGHT - COIN_SIZE)
-            game_state['coins'].append((new_coin_x, new_coin_y))
-
-        # Send the updated game state to all players
-        game_update = pickle.dumps(game_state)
-        for connection in clients.values():
-            connection.send(game_update)
-        
-        # Check if any player has won
-        if len(game_state['players']) > 1:
-            if max(game_state['player_scores'].values()) >= WINNING_SCORE:
-                for player_id, score in game_state['player_scores'].items():
-                    if score >= WINNING_SCORE:
-                        break
-                for connection in clients.values():
-                    connection.send(pickle.dumps(player_id))
-
-    # Remove the player from the game state and close the socket
-    del game_state['players'][player_id]
-    clients.pop(player_id)
+        msg = input_data
+        if type(msg) == str:
+            print(msg)
+            if "REGISTER" in msg:
+                _, mac = msg.split("/")
+                if mac in clients:
+                    conn.send(pickle.dumps("MAC already registered"))
+                else:
+                    clients[mac] = client
+                    client.mac = mac
+                    conn.send(pickle.dumps("OK"))
+            elif "PAY" in msg:
+                _, mac, amount = msg.split("/")
+                if mac in clients:
+                    clients[mac].time += int(amount) * game_price
+                    conn.send(pickle.dumps("OK"))
+                else:
+                    conn.send(pickle.dumps("MAC not registered"))
+            elif "LOGIN" in msg:
+                _, mac = msg.split("/")
+                if mac in logged_in_macs:
+                    conn.send(pickle.dumps("MAC already logged in"))
+                elif mac in clients:
+                    client.mac = mac
+                    client.time = clients[mac].time
+                    clients[mac] = client
+                    conn.send(pickle.dumps("OK"))
+                    time.sleep(0.1)
+                    add_player(client)
+                else:
+                    conn.send(pickle.dumps("MAC not registered"))
+        else:
+            update_player_position(client.player, input_data)
+            
+            coins_to_remove = []
+            for i, (coin_x, coin_y) in enumerate(game_state['coins']):
+                player_x, player_y = game_state['players'][client.player]
+                if player_x < coin_x + COIN_SIZE and player_x + PLAYER_SIZE > coin_x and player_y < coin_y + COIN_SIZE and player_y + PLAYER_SIZE > coin_y:
+                    coins_to_remove.append(i)
+                    game_state['player_scores'][client.player] = game_state['player_scores'].get(client.player, 0) + 1
+            
+            for i in coins_to_remove:
+                game_state['coins'].pop(i)
+                new_coin_x = random.randint(0, WIDTH - COIN_SIZE)
+                new_coin_y = random.randint(0, HEIGHT - COIN_SIZE)
+                game_state['coins'].append((new_coin_x, new_coin_y))
+            
+            game_update = pickle.dumps(game_state)
+            for connection in clients.values():
+                connection.conn.send(game_update)
+            
+            # if len(game_state['players']) > 1:
+            #     if max(game_state['player_scores'].values()) >= WINNING_SCORE:
+            #         for client.player, score in game_state['player_scores'].items():
+            #             if score >= WINNING_SCORE:
+            #                 break
+            #         for connection in clients.values():
+            #             connection.send(pickle.dumps(client.player))
+    
+    del game_state['players'][client.player]
+    clients.pop(client.player)
     conn.close()
 
-def initialize_player_scores():
-        for player_id in game_state['players']:
-            game_state['player_scores'][player_id] = 0
-
-def main():
+def server_loop():
+    global active_clients
     print("> Server is starting...")
     
-    # Create the server socket
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(ADDR)
 
     server.listen()
     print(f"> Server is listening on {IP}:{PORT}")
-    print(f"> [Active Connections] {threading.active_count() - 1}")
-
-    # Accept and handle client connections
+    print(f"> [Active Connections] {active_clients}")
+    
     player_id_counter = 0
-    # initialize_player_scores()  # Initialize player scores
 
     while True:
         conn, addr = server.accept()
+        addr = "{addr[0]}:{addr[1]}"
 
-        player_id = player_id_counter
-        clients[player_id] = conn
-        player_id_counter += 1
+        current_client = Client(conn, addr, 0)
+        active_clients += 1
 
-        # Initialize player's position
-        player_x = random.randint(0, WIDTH - PLAYER_SIZE)
-        player_y = random.randint(0, HEIGHT - PLAYER_SIZE)
-        game_state['players'][player_id] = (player_x, player_y)
+        # player_id = player_id_counter
+        # clients[player_id] = conn
+        # player_id_counter += 1
 
-        
-        display_color = (random.randint(10, 255) for _ in range(3))
-        game_state['color'][player_id] = tuple(display_color)
-        game_state['player_scores'][player_id] = game_state['player_scores'].get(player_id, 0)
-
-        client_thread = threading.Thread(target=handle_client, args=(conn, player_id))
+        client_thread = threading.Thread(target=handle_client, args=(current_client, ))
         client_thread.start()
 
         print(f"> [Active Connections] {threading.active_count() - 1}")
 
+def main():
+    
+    server_thread = threading.Thread(target=server_loop)
+    server_thread.start()
+
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\n> Server stopped.")
+        os._exit(0)
