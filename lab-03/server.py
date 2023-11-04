9270bdc19e722bba64c2bb5ce5f33f92a7b46e1f
Siddesh Devarakonda <siddeshdslr@gmail.com>
2023-11-04 14:48:05 +0530

    Multiple clients connection

--- lab-03/server.py
+++ lab-03/server.py
@@ -1,39 +1,94 @@
 import socket
 import threading
+import sys
 
 IP = socket.gethostbyname(socket.gethostname())
-PORT = 8007
+PORT = 8308
 ADDR = (IP, PORT)
 SIZE = 1024
 FORMAT = "UTF-8"
 DISCONNECT_MSG = "disconnect"
+clients = []
+
+def send_message(msg, client):
+    conn = None
+    for c in clients:
+        if c[1] == client:
+            conn = c[0]
+            break
+    if conn is None:
+        return False
+    try:
+        conn.send(msg.encode(FORMAT))
+    except OSError:
+        return False
+    return True
 
 def handleClient(conn, addr):
-    print(f"> New connection with {addr[0]}:{addr[1]} is connected.")
+    print(f"\r> New connection {addr[0]}:{addr[1]} is connected.")
+
     connected = True
     while connected:
         recv_msg = conn.recv(SIZE).decode(FORMAT)
-        print(f"> Message from client {addr[0]}:{addr[1]} : {recv_msg}")
+        if not recv_msg:
+            print(f"\r> Client {addr[0]}:{addr[1]} is disconnected.")
+            print("\nSend message to (ip:port) : ", end="")
+            sys.stdout.flush()
+            break
+
+        print(f"\r> Message from client {addr[0]}:{addr[1]} : {recv_msg}")
         send_msg = f"MSG was received by server"
+
         if recv_msg.lower() == DISCONNECT_MSG:
             send_msg += f"\n\t - Server is disconnected"
             connected = False
+            for c in clients:
+                if c[1] == addr:
+                    clients.remove(c)
+                    break
 
-        conn.send(send_msg.encode(FORMAT))
+        try:
+            conn.send(send_msg.encode(FORMAT))
+        except BrokenPipeError:
+            print(f"\r> Client {addr[0]}:{addr[1]} is disconnected.")
+            connected = False
+        print("\nSend message to (ip:port) : ", end="")
+        sys.stdout.flush()
+    print(f"\r> Connection {addr[0]}:{addr[1]} is disconnected.")
     conn.close()
+    
+def sendMsg():
+    while True:
+        client_addr = input("\nSend message to (ip:port) : ")
+        client_addr = (client_addr.split(":")[0], int(client_addr.split(":")[1]))
+
+        msg_input = input("Enter your message : ")
+
+        if not send_message(msg_input, client_addr):
+            print(f"Message was not sent to {client_addr[0]}:{client_addr[1]}")
 
 def main():
     print("> Server is starting...")
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
     server.bind(ADDR)
+
     server.listen()
     print(f"Server is listening on {IP}:{PORT}")
 
+    thread1 = threading.Thread(target=sendMsg, args=())
+    thread1.start()
+
     while True:
         conn, addr = server.accept()
-        thread = threading.Thread(target=handleClient, args=(conn, addr))
-        thread.start()
-        print(f"> Current Active Connections : {threading.active_count() - 1}")
+        clients.append((conn, addr))
+
+        thread2 = threading.Thread(target=handleClient, args=(conn, addr))
+        thread2.start()
+
+        print(f"> Current Active Connections : {threading.active_count() - 2}")
+        print("\nSend message to (ip:port) : ", end="")
+        sys.stdout.flush()
 
 if __name__ == "__main__":
     main()
